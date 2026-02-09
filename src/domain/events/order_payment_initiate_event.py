7eff2f7f2131ba36430ff1493c892c5895241ea0
shafeeque <shafeeqsha06@gmail.com>
2026-02-09 14:22:39 +0530

    feat(event): add domain order payment intiated event type

---
+++ src/domain/events/order_payment_initiate_event.py
@@ -0,0 +1,53 @@
+from typing import Any, Type, TypeVar, Dict
+from pydantic import BaseModel, Field, ValidationError
+
+from .base_event_dto import BaseEventDto
+
+T = TypeVar("T", bound="OrderPaymentInitiatedEventDto")
+
+class OrderPaymentInitiatedPayload(BaseModel):
+    order_id: str = Field(..., description="ID of the payment is required")
+    payment_id: str = Field(..., description="Payment Id of the payment is required")
+    user_id: str = Field(..., description="User Id of the payment is required")
+    provider: str = Field(..., description="Provider of the payment is required")
+    provider_order_id: str = Field(..., description="Provider order Id  of the payment is required")
+    payment_status: str = Field(..., description="status of the payment is required")
+
+class OrderPaymentInitiatedEventDto(BaseEventDto[OrderPaymentInitiatedPayload]):
+    @classmethod
+    def from_payload(cls: Type[T], event: Dict[str, Any]) -> T:
+        payload_data = event.get("payload", {})
+        if not isinstance(payload_data, dict):
+            raise ValueError("Event payload must be a dictionary.")
+
+        try:
+            payload_obj = OrderPaymentInitiatedPayload(
+                order_id=payload_data["orderId"],
+                payment_id=payload_data["paymentId"],
+                user_id=payload_data["userId"],
+                provider=payload_data["provider"],
+                provider_order_id=payload_data["providerOrderId"],
+                payment_status=payload_data["paymentStatus"]
+            )
+        except KeyError as e:
+            raise ValueError(f"Missing required payload field: {e}")
+        except ValidationError as e:
+            raise ValueError(f"Invalid payload data: {e}")
+
+        try:
+            timestamp = int(event["timestamp"])
+            event_id = str(event["eventId"])
+            event_type = str(event["eventType"])
+            source = str(event["source"])
+        except KeyError as e:
+            raise ValueError(f"Missing required event field: {e}")
+        except (TypeError, ValueError) as e:
+            raise ValueError(f"Invalid event field type: {e}")
+        
+        return cls(
+            timestamp=timestamp,
+            event_id=event_id,
+            event_type=event_type,
+            payload=payload_obj,
+            source=source,
+        )

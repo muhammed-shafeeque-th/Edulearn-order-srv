a63075a9d00387c855acf88ebc200ada679ef70a
shafeeque <shafeeqsha06@gmail.com>
2026-02-09 13:39:14 +0530

    feat(order-item): update order items domain entity

--- src/domain/entities/order_items.py
+++ src/domain/entities/order_items.py
@@ -2,19 +2,22 @@ from dataclasses import dataclass
 from typing import Any
 
 
+
 @dataclass
 class OrderItem:
+    id: str
     course_id: str
     price: float
 
     @classmethod
     def create(
-        cls, course_id: str, price: float
+        cls, id: str, course_id: str, price: float
     ) -> "OrderItem":
         return cls(
+            id=id,
             course_id=course_id,
             price=price,
         )
 
     def to_dict(self) -> dict[str, Any]:
-        return {"course_id": self.course_id, "price": self.price}
+        return {"id": self.id, "course_id": self.course_id, "price": self.price}

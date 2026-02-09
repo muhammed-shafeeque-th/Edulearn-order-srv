04fab87a2c5ac637cc0d7b128d0b7c9ac9dc4d23
shafeeque <shafeeqsha06@gmail.com>
2026-02-09 13:18:54 +0530

    feat(db-model): add psycopg database order model

--- src/infrastructure/database/models/order_model.py
+++ src/infrastructure/database/models/order_model.py
@@ -40,20 +40,21 @@ class OrderItemModel(Base):
         Index("idx_order_items_course_id", "course_id"),
     )
 
-    def map_to_domain(self) -> OrderItem:
-        return OrderItem(
-            course_id=self.__dict__['course_id'],
-            price=self.__dict__['price']
-        )
+    # def map_to_domain(self) -> OrderItem:
+    #     return OrderItem(
+    #         id=self.__dict__["id"],
+    #         course_id=self.__dict__['course_id'],
+    #         price=self.__dict__['price']
+    #     )
 
-    @classmethod
-    def from_domain(cls, order_item: OrderItem, order_id: str) -> "OrderItemModel":
-        return cls(
-            id=str(uuid4()),
-            order_id=order_id,
-            course_id=order_item.course_id,
-            price=order_item.price
-        )
+    # @classmethod
+    # def from_domain(cls, order_item: OrderItem, order_id: str) -> "OrderItemModel":
+    #     return cls(
+    #         id=str(uuid4()),
+    #         order_id=order_id,
+    #         course_id=order_item.course_id,
+    #         price=order_item.price
+    #     )
 
 
 class PaymentDetailsModel(Base):
@@ -81,36 +82,39 @@ class PaymentDetailsModel(Base):
         Index("idx_payment_details_provider_order_id", "provider_order_id"),
     )
 
-    def map_to_domain(self) -> PaymentDetails:
-        return PaymentDetails(
-            payment_id=self.__dict__["payment_id"],
-            provider=self.__dict__["provider"],
-            provider_order_id=self.__dict__["provider_order_id"],
-            payment_status=self.__dict__["payment_status"],
-            updated_at=self.__dict__["updated_at"]
-        )
+    # def map_to_domain(self) -> PaymentDetails:
+    #     return PaymentDetails(
+    #         id=self.__dict__["id"],
+    #         payment_id=self.__dict__["payment_id"],
+    #         provider=self.__dict__["provider"],
+    #         provider_order_id=self.__dict__["provider_order_id"],
+    #         payment_status=self.__dict__["payment_status"],
+    #         updated_at=self.__dict__["updated_at"]
+    #     )
 
-    @classmethod
-    def from_domain(cls, payment_details: PaymentDetails, order_id: str) -> "PaymentDetailsModel":
-        return cls(
-            id=str(uuid4()),
-            order_id=order_id,
-            payment_id=payment_details.payment_id,
-            provider=payment_details.provider,
-            provider_order_id=payment_details.provider_order_id,
-            payment_status=payment_details.payment_status,
-            updated_at=payment_details.updated_at
-        )
+    # @classmethod
+    # def from_domain(cls, pd: PaymentDetails, order_id: str):
+    #     return cls(
+    #         id=str(uuid4()) if not getattr(pd, "id", None) else pd.id,
+    #         order_id=order_id,
+    #         payment_id=pd.payment_id,
+    #         provider=pd.provider,
+    #         provider_order_id=pd.provider_order_id,
+    #         payment_status=pd.payment_status,
+    #         updated_at=pd.updated_at or datetime.utcnow()
+    #     )
 
 
 class OrderModel(Base):
     __tablename__ = "orders"
 
     id = Column(String, primary_key=True, index=True)
+    idempotency_key = Column(String, unique=True, index=True, nullable=True)
     user_id = Column(String, nullable=False)
     amount = Column(Float, nullable=False)
     currency = Column(String, default="INR")
     sub_total = Column(Integer, default=0, nullable=False)
+    sales_tax = Column(Integer, default=0, nullable=False)
     discount = Column(Integer, nullable=True)
     status = Column(String, default="PENDING")
     created_at = Column(DateTime(timezone=True),
@@ -140,42 +144,46 @@ class OrderModel(Base):
         Index("idx_orders_status", "status"),
     )
     
-    def map_to_domain(self) -> Order:
-        # Convert order items
-        items = [item_model.map_to_domain() for item_model in self.items]
+    # def map_to_domain(self) -> Order:
+    #     # Convert order items
+    #     items = [item_model.map_to_domain() for item_model in self.items]
 
-        # Convert payment details if exists
-        payment_details = None
-        if self.payment_details:
-            payment_details = self.payment_details.map_to_domain()
+    #     # Convert payment details if exists
+    #     payment_details = None
+    #     if self.payment_details:
+    #         payment_details = self.payment_details.map_to_domain()
 
-        # Create Money value object
-        amount = Money(amount=self.__dict__[
-                             "amount"], currency=self.__dict__["currency"])
+    #     # Create Money value object
+    #     amount = Money(amount=self.__dict__[
+    #                          "amount"], currency=self.__dict__["currency"])
 
-        return Order(
-            id=self.__dict__["id"],
-            user_id=self.__dict__["user_id"],
-            # course_ids=list(self.__dict__["course_ids"]) if self.__dict__["course_ids"] else [],
-            items=items,
-            amount=amount,
-            sub_total=self.__dict__['sub_total'],
-            discount=self.__dict__['discount'],
-            status=OrderStatus(self.__dict__["status"]),
-            payment_details=payment_details,
-            created_at=self.__dict__["created_at"],
-            updated_at=self.__dict__["updated_at"],
-        )
+    #     return Order(
+    #         id=self.__dict__["id"],
+    #         user_id=self.__dict__["user_id"],
+    #         idempotency_key=self.__dict__["idempotency_key"],
+    #         # course_ids=list(self.__dict__["course_ids"]) if self.__dict__["course_ids"] else [],
+    #         items=items,
+    #         sales_tax=self.__dict__["sales_tax"],
+    #         amount=amount,
+    #         sub_total=self.__dict__['sub_total'],
+    #         discount=self.__dict__['discount'],
+    #         status=OrderStatus(self.__dict__["status"]),
+    #         payment_details=payment_details,
+    #         created_at=self.__dict__["created_at"],
+    #         updated_at=self.__dict__["updated_at"],
+    #     )
 
-    @classmethod
-    def from_domain(cls, order: Order) -> "OrderModel":
-        return cls(
-            id=order.id,
-            user_id=order.user_id,
-            amount=order.amount.amount,
-            currency=order.amount.currency,
-            discount=order.discount,
-            status=order.status.value,
-            created_at=order.created_at,
-            updated_at=order.updated_at,
-        )
+    # @classmethod
+    # def from_domain(cls, order: Order) -> "OrderModel":
+    #     return cls(
+    #         id=order.id,
+    #         user_id=order.user_id,
+    #         idempotency_key=order.idempotency_key,
+    #         amount=order.amount.amount,
+    #         currency=order.amount.currency,
+    #         discount=order.discount,
+    #         sales_tax=order.sales_tax,
+    #         status=order.status.value,
+    #         created_at=order.created_at,
+    #         updated_at=order.updated_at,
+    #     )

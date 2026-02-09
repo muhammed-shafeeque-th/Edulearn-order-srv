7de7492474dba73346af08351c08575ef22c0c93
shafeeque <shafeeqsha06@gmail.com>
2026-02-09 13:51:51 +0530

    feat(kafka): update kafka producer interface

--- src/application/interfaces/kafka_producer_interface.py
+++ src/application/interfaces/kafka_producer_interface.py
@@ -1,11 +1,14 @@
 from abc import ABC, abstractmethod
 from fastavro.types import Schema
 
-class IKafkaProducer(ABC):
+from typing import TypeVar, Generic
+
+T = TypeVar("T")
+
+class IKafkaProducer(ABC, Generic[T]):
     @abstractmethod
-    async def publish_event(self, topic: str, event: dict, schema: Schema | None) -> None:
+    async def publish_event(self, topic: str, event: T, schema: Schema | None) -> None:
         pass
-    
     @abstractmethod
     async def start(self) -> None:
         pass

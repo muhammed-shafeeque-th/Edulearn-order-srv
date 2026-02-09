c05b327ee83d12daeafa0d2dcadfddf470ec1233
shafeeque <shafeeqsha06@gmail.com>
2026-02-09 13:36:17 +0530

    feat(order-failed): update order failed use-case by using orderFailedEvent entity instead of default mapping

--- src/application/use_cases/order/order_failed_use_case.py
+++ src/application/use_cases/order/order_failed_use_case.py
@@ -1,13 +1,27 @@
-from src.application.dtos.order_payment_failure_dto import OrderPaymentFailureDto
+from datetime import datetime, timezone
+from src.domain.events.order_failed_event import OrderFailedEventType, OrderFailedEvent
+from src.application.interfaces.kafka_producer_interface import IKafkaProducer
+from src.application.interfaces.logging_interface import ILoggingService
+from src.application.interfaces.metrics_interface import IMetricsService
+from src.application.interfaces.redis_interface import IRedisService
+from src.domain.repositories.order_repository import IOrderRepository
+from src.domain.events.order_payment_failure_event import OrderPaymentFailureEventDto
 from src.domain.entities.payment_details import PaymentDetails, PaymentStatus
 from src.shared.events.topics import EVENT_TOPICS
 from src.infrastructure.database.database import get_db
 from src.domain.exceptions.exceptions import OrderNotFoundException
 from tenacity import retry, stop_after_attempt, wait_exponential
+from uuid import uuid4
 
 
 class OrderFailedUseCase:
-    def __init__(self, order_repository, kafka_producer, redis, logging_service, metrics_service):
+    def __init__(self,
+                 order_repository: IOrderRepository,
+                 kafka_producer: IKafkaProducer[OrderFailedEventType],
+                 redis: IRedisService,
+                 logging_service: ILoggingService,
+                 metrics_service: IMetricsService):
+
         self.order_repository = order_repository
         self.kafka_producer = kafka_producer
         self.redis = redis
@@ -15,40 +29,44 @@ class OrderFailedUseCase:
         self.metrics = metrics_service
 
     @retry(stop=stop_after_attempt(3), wait=wait_exponential(multiplier=1, min=1, max=10))
-    async def execute(self, dto: OrderPaymentFailureDto):
-        self.logger.info(f"Processing failed payment for order {dto.order_id}")
+    async def execute(self, dto: OrderPaymentFailureEventDto):
+        payload = dto.payload
+        
+        self.logger.info(f"Processing failed payment for order {payload.order_id}")
 
         async with get_db() as session:
-            order = await self.order_repository.find_by_id(dto.order_id, session)
+            order = await self.order_repository.find_by_id(payload.order_id, session)
             if not order:
                 raise OrderNotFoundException(
-                    f"Order not found: {dto.order_id}")
+                    f"Order not found: {payload.order_id}")
 
             if not order.payment_details:
                 payment_details = PaymentDetails(
-                    payment_id=dto.payment_id,
-                    provider=dto.provider,
-                    provider_order_id=dto.provider_order_id,
-                    payment_status="pending",
+                    id=str(uuid4()),
+                    payment_id=payload.payment_id,
+                    provider=payload.provider,
+                    provider_order_id=payload.provider_order_id,
+                    updated_at=datetime.fromtimestamp(
+                        int(dto.timestamp) / 1000, tz=timezone.utc),
+                    payment_status="failed",
                 )
                 order.set_payment_details(payment_details)
-            else:
-                order.payment_details.setStatus("pending")
 
             order.mark_failed()
             await self.order_repository.save(order, session)
 
         await self.kafka_producer.publish_event(
             EVENT_TOPICS.ORDER_COURSE_FAILED.value,
-            event={
-                "orderId": order.id,
-                "userId": order.user_id,
-                "items": [{"course_id": i.course_id, "price": i.price} for i in order.items],
-                "amount": order.amount.amount,
-                "currency": order.amount.currency,
-            },
+            event=OrderFailedEvent(
+                orderId=order.id,
+                userId=order.user_id,
+                items=[{"courseId": i.course_id, "price": i.price}
+                       for i in order.items],
+                amount=order.amount.amount,
+                currency=order.amount.currency,
+            ).to_dict(),
             schema=None,
         )
 
-        self.logger.warning(f"❌ Order {dto.order_id} marked as FAILED")
+        self.logger.warning(f"Order {payload.order_id} marked as FAILED")
         return

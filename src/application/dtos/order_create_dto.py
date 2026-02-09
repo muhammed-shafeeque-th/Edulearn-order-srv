5074dc74c6c4dab9e01d56c2a4f4dea18e553df8
shafeeque <shafeeqsha06@gmail.com>
2026-02-09 13:56:25 +0530

    feat(dto): add grpc request validation dto with pydantic schema

--- src/application/dtos/order_create_dto.py
+++ src/application/dtos/order_create_dto.py
@@ -5,6 +5,9 @@ from datetime import datetime
 class OrderCreateDto(BaseModel):
     user_id: str = Field(..., description="ID of the user placing the order")
     course_ids: list[str] = Field(..., description="List of course IDs to order")
+    coupon_code: str | None = Field(
+        default=None, description="ID of coupon if Present"
+    )
 
     @field_validator("course_ids")
     def validate_course_ids(cls, value):
